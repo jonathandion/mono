9ef032b4fd10dcb9226c2f49450a55e857856aa3
jonathandion <jonathandionalary@gmail.com>
2023-09-04 19:32:00 -0400

    feature/cast-package - wip

--- examples/cast-react-example/src/main.tsx
+++ examples/cast-react-example/src/main.tsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/default
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { CastDemo } from './cast/cast.component.tsx'
+import { CastDemo } from './cast/cast.component'
 import { CastProvider } from '@jdion/cast-react'
-import { castInstance } from './cast/cast.instance.tsx'
+import { castInstance } from './cast/cast.instance'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
