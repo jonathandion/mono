dd76194c35a7814ac737600ebb31d04f63a87e12
jonathandion <jonathandionalary@gmail.com>
2023-09-18 20:17:09 -0400

    Updates

---
+++ static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/cast":["static\u002Fchunks\u002Fpages\u002Fcast.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcast"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
