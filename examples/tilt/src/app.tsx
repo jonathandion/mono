28e6e2cad011bcaddb0bd037b96e4d1745db4a49
Jon <jonathandionalary@gmail.com>
2023-10-15 14:25:32 +0100

    Updated @jdion/tilt-react demo

--- examples/tilt/src/app.tsx
+++ examples/tilt/src/app.tsx
@@ -4,7 +4,19 @@ import './app.css'
 export const App = () => {
   return (
     <div className="container mx-auto p-20 flex items-center justify-center">
-      <Tilt className="h-[250px] w-[250px] drop-shadow-2xl text-6xl flex items-center justify-center max-w-sm p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 shadow">👽</Tilt>
+      <Tilt
+        className="flex h-[200px] w-[200px] content-center items-center justify-center rounded-xl bg-gradient-to-r from-violet-500 to-fuchsia-500"
+        style={{
+          boxShadow: '0 48px 60px 0 rgba(2,14,26,.24)',
+          transformStyle: 'preserve-3d',
+        }} 
+      >
+        <div style={{
+          transform: 'translateZ(60px)',
+        }}>
+          <span className="text-6xl" role="img" aria-label="alien">👽</span>
+        </div>
+      </Tilt>
     </div>
   )
 }
