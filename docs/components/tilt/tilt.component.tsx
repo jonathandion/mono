0a4c557cf67cb72acf36455eb2bfeda289ef805c
jonathandion <jonathandionalary@gmail.com>
2023-10-15 14:54:11 +0100

    feature/tilt-react-demo - wip

--- docs/components/tilt/tilt.component.tsx
+++ docs/components/tilt/tilt.component.tsx
@@ -2,10 +2,22 @@ import { Tilt } from '@jdion/tilt-react'
 
 export const TiltComponent = () => {
   return (
-    <Tilt className="h-[150px] mx-auto w-[300px] flex items-center justify-center rounded-xl border border-neutral-800">
-      <div className="flex items-center">
-        <div className="text-white text-4xl font-bold uppercase">Tilt ✨</div>
-      </div>
-    </Tilt>
+    <div className="container mx-auto p-20 flex items-center justify-center">
+
+      <Tilt
+        className="flex h-[150px] w-[300px] content-center items-center justify-center rounded-xl border border-neutral-800"
+        style={{
+          transformStyle: 'preserve-3d',
+        }}
+      >
+        <div style={{
+          transform: 'translateZ(60px)',
+        }}>
+          <span className="text-white text-4xl font-bold uppercase" role="img" aria-label="alien">
+            Tilt ✨
+          </span>
+        </div>
+      </Tilt>
+    </div>
   )
 }
